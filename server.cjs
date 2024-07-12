e8181aec851afef799f3a3cfc9a39fad394d5d17
Daniel Roth <110140817+Dcroth99@users.noreply.github.com>
2024-07-11 14:40:50 -1000

    live3

--- server.cjs
+++ server.cjs
@@ -1,11 +1,6 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
+const express = require('express');
+const path = require('path');
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
